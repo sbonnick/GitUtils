cfc9609ee36fd7423ccf506ad62f6406f121099f
sbonnick <stewart.bonnick@autoclavestudios.com>
2015-03-19 12:30:10 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # GitUtils
 Misc. Git utilities to simplify working in Git targeting large projects spanning multiple repositories
+
+## pull-all
+This is a simple tool that will iterate over all the files in the current directory performing the following tasks:
+
+1. detect if the sub directory is a Git project
+  1. display the current (short form) Git status
+  2.  execute a pull with pruning on the given local repository
+  
+This tool is ideal for larger projects that are spread across multiple Git repositories and cloned locally in the same "working" folder
---
+++ pull-all.bat
@@ -0,0 +1,59 @@
+@echo off
+call :color_init
+setlocal EnableDelayedExpansion
+
+REM Iterates through sub directories and performs git status and pull (with prune)
+echo.
+echo --------------------------------------------------------------------------------
+FOR /D %%i in (*) DO (
+	set name= %%i                                       
+	call :detect_remote %%i
+	set remote=                                       !remote! 
+	call :color 0a "!name:~0,40!"
+	echo !remote:~-40!
+	echo --------------------------------------------------------------------------------
+	echo.
+	IF !isGit! EQU 1 (
+		call :color 03 "+++ Status"
+		echo.
+		echo.
+		git -C %%i status -s
+		echo.
+		call :color 03 "+++ Pull"
+		echo.
+		echo.
+		git -C %%i pull --prune
+		echo.
+	)
+	echo --------------------------------------------------------------------------------
+)
+DEL X
+exit /b
+
+
+:detect_remote 
+IF EXIST %~1\.git (
+	git -C %~1 rev-parse --symbolic-full-name --abbrev-ref @{u} > temp_remote.txt
+	set isGit=1
+) ELSE (
+	echo.|set /P ="NOT A GIT REPOSITORY" > temp_remote.txt
+	set isGit=0
+)
+set /p remote=<temp_remote.txt
+DEL temp_remote.txt
+exit /b
+
+
+:color_init
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+  set "DEL=%%a"
+)
+echo.|set /P ="." > X
+exit /b
+
+:color
+set "param=^%~2" !
+set "param=!param:"=\"!"
+findstr /p /A:%1 "." "!param!\..\X" nul
+<nul set /p ".=%DEL%%DEL%%DEL%%DEL%%DEL%%DEL%%DEL%"
+exit /b
