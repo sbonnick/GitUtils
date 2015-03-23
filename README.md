d8202aa7c7133e38d4fd8d849d407129d1bd5bc9
sbonnick <stewart.bonnick@autoclavestudios.com>
2015-03-23 13:39:58 -0400

    Refactored pull-all to support optional prints and ignored files

--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 Misc. Git utilities to simplify working in Git targeting large projects spanning multiple repositories
 
 ## pull-all
+
+Usage: pull-all [-i]
+-i : include listing ignored files of a project
+
 This is a simple tool that will iterate over all the files in the current directory performing the following tasks:
 
 1. detect if the sub directory is a Git project
   1. display the current (short form) Git status
-  2.  execute a pull with pruning on the given local repository
+  2. optionally show ignored files (short form). the "-i" needs to be specified
+  3. execute a pull with pruning on the given Git project
   
 This tool is ideal for larger projects that are spread across multiple Git repositories and cloned locally in the same "working" folder
--- pull-all.bat
+++ pull-all.bat
@@ -2,8 +2,13 @@
 call :color_init
 setlocal EnableDelayedExpansion
 
-REM Iterates through sub directories and performs git status and pull (with prune)
-REM Author: Stewart Bonnick (stewart.bonnick@autoclavestudios.com)
+:: Iterates through sub directories and performs git status and pull (with prune)
+:: Author: Stewart Bonnick (stewart.bonnick@autoclavestudios.com)
+
+
+::-----------------------------------------------------------------------
+::|                          Parameters                                 |
+::-----------------------------------------------------------------------
 
 set incIgnore=0
 
@@ -15,6 +20,11 @@ SHIFT
 GOTO parse
 :endparse
 
+
+::-----------------------------------------------------------------------
+::|                          Main Code                                  |
+::-----------------------------------------------------------------------
+
 echo.
 echo --------------------------------------------------------------------------------
 FOR /D %%i in (*) DO (
@@ -24,33 +34,23 @@ FOR /D %%i in (*) DO (
 	call :color 0a "!name:~0,40!"
 	echo !remote:~-40!
 	echo --------------------------------------------------------------------------------
-	echo.
 	IF !isGit! EQU 1 (
-		call :color 03 "+++ Status"
-		echo.
-		echo.
-		git -C %%i status -s
-		echo.
-		if !incIgnore! EQU 1 (
-			call :color 03 "+++ Ignored Files"
-			echo.
-			echo.
-			git -C %%i status -s --ignored
-			echo.
-		)
-		call :color 03 "+++ Pull"
-		echo.
-		echo.
-		git -C %%i pull --prune
-		echo.
+		call :get_status %%i
+		call :get_status %%i !incIgnore!
+		call :get_pull %%i
 	)
+	echo.
 	echo --------------------------------------------------------------------------------
 )
-DEL X
+call :color_dispose
 exit /b
 
 
-:detect_remote 
+::-----------------------------------------------------------------------
+::|                          Functions                                  |
+::-----------------------------------------------------------------------
+
+:detect_remote -- <Folder>
 IF EXIST %~1\.git (
 	git -C %~1 rev-parse --symbolic-full-name --abbrev-ref @{u} > temp_remote.txt
 	set isGit=1
@@ -62,9 +62,38 @@ set /p remote=<temp_remote.txt
 DEL temp_remote.txt
 exit /b
 
+:get_status -- <Folder> <Include Ignore Print>
+set exe=git -C %~1 status -s
+set label="Status"
+IF [%~2] == [0] ( exit /b )
+IF [%~2] == [1]  (
+	set exe=git -C %~1 status -s --ignored
+	set label="Ignored Files"
+)
+!exe! > temp_status.txt
+for /f %%t in ("temp_status.txt") do set size=%%~zt
+if !size! GTR 0 (
+	call :format_label !label!
+	!exe!
+)
+DEL temp_status.txt
+exit /b
 
-REM Below code or ideas are derived from various other peoples work. Originators are unknown -
-REM as there are different flavours of the same snippets. All licences however are MIT or Apache 2.0
+:get_pull -- <Folder>
+call :format_label "Pull"
+git -C %~1 pull --prune
+exit /b
+
+:format_label -- <Label>
+echo.
+call :color 03 "+++ %~1"
+echo.
+echo.
+exit /b
+
+
+:: Below code or ideas are derived from various other peoples work. Originators are unknown -
+:: as there are different flavours of the same snippets. All licences however are MIT or Apache 2.0
 
 :color_init
 for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
@@ -73,7 +102,11 @@ for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1)
 echo.|set /P ="." > X
 exit /b
 
-:color
+:color_dispose
+DEL X
+exit /b
+
+:color  -- <Color Code> <Label>
 set "param=^%~2" !
 set "param=!param:"=\"!"
 findstr /p /A:%1 "." "!param!\..\X" nul
